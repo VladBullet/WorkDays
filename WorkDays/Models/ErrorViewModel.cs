4dc673c9413a02ce709b4bf2fbe2e2ef814364f3
Vlad Bulete <vlad.bulete@proplanet.nl>
2020-07-01 13:18:33 +0300

    changes

---
+++ WorkDays/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkDays.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
